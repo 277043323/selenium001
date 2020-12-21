bad76fc9da4265b19f96024efd913764604eff7b
277043323 <277043323@qq.com>
2020-12-21 16:35:40 +0800

    testDemo

--- test/testDemo.py
+++ test/testDemo.py
@@ -4,7 +4,7 @@ from automationcodejiang.login.main import test
 
 class Test:
     def setup(self):
-        self.main=test()
+        self.main  = test()
 
     def test(self):
         self.main.Main().deng().couser().action()
